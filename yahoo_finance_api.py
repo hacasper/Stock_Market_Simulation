a31f7fc515a4b923f23572c35edec8f9cc1960bd
Hannah Casper <hannahcasper8@gmail.com>
2021-01-11 11:37:53 -0800

    Took out API key

--- yahoo_finance_api.py
+++ yahoo_finance_api.py
@@ -1,13 +1,12 @@
 import http.client
 import ssl
-import datetime 
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 conn = http.client.HTTPSConnection("apidojo-yahoo-finance-v1.p.rapidapi.com")
 
 headers = {
-    'x-rapidapi-key': "720b2beefemsh17802dccb5f8d6dp1efcc4jsn87b74001509d",
+    'x-rapidapi-key': "ENTER API KEY HERE",
     'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com"
     }
 
@@ -17,3 +16,4 @@ res = conn.getresponse()
 data = res.read()
 
 print(data.decode("utf-8"))
+
