3d02e51f86befe2889665a109e87926eaee9e731
Lionel Hanke <lionel.hanke@gmail.com>
2021-01-22 15:07:54 -0800

    preds

---
+++ Predictions.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 22 14:52:27 2021
+
+@author: Lionel
+"""
+
+import pandas as pd
+import datetime as dt
+import math
+import csv  
+import numpy as np
+import matplotlib.pyplot as plt
+
+
