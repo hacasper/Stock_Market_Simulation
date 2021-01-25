dfa85cfddcecf045f84d06361144536c59fa10e8
Hannah Casper <hannahcasper8@gmail.com>
2021-01-25 08:41:36 -0800

    adding market file

---
+++ market.py
@@ -0,0 +1,21 @@
+
+
+#Market and trader objects from
+from Classes import trader 
+from Classes import market
+
+#stock market compoonent of trading model
+
+#handles collecting order and processing selling / buying of stocks
+
+#Variables
+#number of shares to be purchased/sold
+n=0
+
+#buy/hold/sell decision
+order = 0
+
+
+def getStockOrder (n, order):
+    print 0
+
