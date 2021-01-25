77f4fe1a1d6b310dd1ab8e968280bf5ed42002d9
Hannah Casper <hannahcasper8@gmail.com>
2021-01-25 09:54:05 -0800

    commented out hist variable in market to resolve errors for now

--- market.py
+++ market.py
@@ -1,4 +1,4 @@
-
+import pandas as pd
 
 #Market and trader objects from
 from Classes import trader 
@@ -8,14 +8,27 @@ from Classes import market
 
 #handles collecting order and processing selling / buying of stocks
 
-#Variables
-#number of shares to be purchased/sold
-n=0
-
-#buy/hold/sell decision
-order = 0
+stockPrice = 0
 
 
+bitData = pd.read_csv ("Bitcoin_Min_Jan20.csv")
+
+def getStockPrice(date):
+    infoAtDate = list(bitData.loc[bitData['time_period_start'] == date])
+    print(infoAtDate)
+    stockPrice = infoAtDate[5]
+    print(str(stockPrice))
+
+    
 def getStockOrder (n, order):
-    print 0
+    #let order be value 1, 0, -1
+    #let n be int for number of shares
+    if order == 0:
+        print("Trader is holding")
+    if order == 1:
+        print("Trader is buying")
+    if order == -1:
+        print("Trader is selling")
 
+getStockOrder(1, 1)
+getStockPrice('2020-01-01T00:00:00.0000000Z')
